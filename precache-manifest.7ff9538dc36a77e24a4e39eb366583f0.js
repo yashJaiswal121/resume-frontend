daa7e0991daaaa619c7f00340cb19ffe9b2437d1
unknown <LPT-2585@LPT2585.newgen.co.in>
2021-01-10 21:35:20 +0530

    Updates

---
+++ precache-manifest.7ff9538dc36a77e24a4e39eb366583f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d9cf7b6d220e3cf1ee8c3ca57f74b5f",
+    "url": "/resume-frontend/index.html"
+  },
+  {
+    "revision": "9a05d477fbd6d7461508",
+    "url": "/resume-frontend/static/css/2.ab922615.chunk.css"
+  },
+  {
+    "revision": "b22d179d7e92b5a33302",
+    "url": "/resume-frontend/static/css/main.933f8601.chunk.css"
+  },
+  {
+    "revision": "9a05d477fbd6d7461508",
+    "url": "/resume-frontend/static/js/2.f87d1aa3.chunk.js"
+  },
+  {
+    "revision": "923a93445da4455d8e7e236ec4fa5442",
+    "url": "/resume-frontend/static/js/2.f87d1aa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b22d179d7e92b5a33302",
+    "url": "/resume-frontend/static/js/main.68e0bf13.chunk.js"
+  },
+  {
+    "revision": "ff5b1f0cd845a8c5e8fd",
+    "url": "/resume-frontend/static/js/runtime-main.ea11821f.js"
+  }
+]);
